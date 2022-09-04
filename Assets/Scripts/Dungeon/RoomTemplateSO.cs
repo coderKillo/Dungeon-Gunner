a1cf3982e42fc941ed47f62d942fd51d13886654
coderKillo <coderKillo@users.noreply.github.com>
2022-09-05 01:04:51 +0200

    add: RoomTemplate ScriptableObject + import finished rooms

---
+++ Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Room_", menuName = "Scriptable Objects/Dungeon/Room")]
+public class RoomTemplateSO : ScriptableObject
+{
+    [HideInInspector] public string guid;
+
+    [Space(10)]
+    [Header("Room Prefab")]
+
+    [Tooltip("The gameobject prefab for the room (this will contain all the tilemaps for the room and environment game objects")]
+
+    public GameObject prefab;
+
+    [HideInInspector] public GameObject previousPrefab; // this is used to regenerate the guid if the so is copied and the prefab is changed
+
+
+    [Space(10)]
+    [Header("Room Configuration")]
+
+    [Tooltip("The room node type SO. The room node types correspond to the room nodes used in the room node graph.  The exceptions being with corridors.  In the room node graph there is just one corridor type 'Corridor'.  For the room templates there are 2 corridor node types - CorridorNS and CorridorEW.")]
+    public RoomNodeTypeSO roomNodeType;
+
+    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room lower bounds represent the bottom left corner of that rectangle. This should be determined from the tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that bottom left corner (Note: this is the local tilemap position and NOT world position")]
+    public Vector2Int lowerBounds;
+
+    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room upper bounds represent the top right corner of that rectangle. This should be determined from the tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that top right corner (Note: this is the local tilemap position and NOT world position")]
+    public Vector2Int upperBounds;
+
+    [Tooltip("There should be a maximum of four doorways for a room - one for each compass direction.  These should have a consistent 3 tile opening size, with the middle tile position being the doorway coordinate 'position'")]
+    [SerializeField] public List<Doorway> doorwayList;
+
+    [Tooltip("Each possible spawn position (used for enemies and chests) for the room in tilemap coordinates should be added to this array")]
+    public Vector2Int[] spawnPositionArray;
+
+    public List<Doorway> GetDoorwayList()
+    {
+        return doorwayList;
+    }
+
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        if (guid == "" || previousPrefab != prefab)
+        {
+            guid = GUID.Generate().ToString();
+            previousPrefab = prefab;
+            EditorUtility.SetDirty(this);
+        }
+
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(doorwayList), doorwayList);
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(spawnPositionArray), spawnPositionArray);
+    }
+
+#endif
+}
