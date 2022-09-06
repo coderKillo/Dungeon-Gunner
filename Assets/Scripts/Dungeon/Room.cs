239b11a2db73a5b9e27cc9d8b844b26f1077c4bf
coderKillo <coderKillo@users.noreply.github.com>
2022-09-06 22:27:07 +0200

    add: Room class

---
+++ Assets/Scripts/Dungeon/Room.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+    public string id;
+    public string templateId;
+
+    public GameObject prefab;
+
+    public RoomNodeTypeSO nodeType;
+
+    public Vector2Int lowerBound;
+    public Vector2Int upperBound;
+    public Vector2Int templateLowerBound;
+    public Vector2Int templateUpperBound;
+
+    public Vector2Int[] spawnPositions;
+
+    public string parentId;
+    public List<string> childIdList;
+
+    public List<Doorway> doorwayList;
+
+    public InstantiatedRoom instantiatedRoom;
+
+    public bool isLit = false;
+    public bool isPositioned = false;
+    public bool isClearedOfEnemies = false;
+    public bool isPreviouslyVisited = false;
+
+    public Room()
+    {
+        childIdList = new List<string>();
+        doorwayList = new List<Doorway>();
+    }
+}
