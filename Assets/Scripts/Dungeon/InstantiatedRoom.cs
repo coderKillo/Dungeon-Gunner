7bf24098bc1f5644f1ca075c33a22c913001ed1d
coderKillo <coderKillo@users.noreply.github.com>
2022-09-09 23:40:31 +0200

    add: block unused doorways

--- Assets/Scripts/Dungeon/InstantiatedRoom.cs
+++ Assets/Scripts/Dungeon/InstantiatedRoom.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -29,7 +27,18 @@ public class InstantiatedRoom : MonoBehaviour
     {
         grid = roomGameObject.GetComponentInChildren<Grid>();
 
-        foreach (var tilemap in roomGameObject.GetComponentsInChildren<Tilemap>())
+        var tilemaps = roomGameObject.GetComponentsInChildren<Tilemap>();
+
+        PopulateTilemaps(tilemaps);
+
+        BlockOffUnusedDoorways(tilemaps);
+
+        collisionTilemap.gameObject.GetComponent<TilemapRenderer>().enabled = false;
+    }
+
+    private void PopulateTilemaps(Tilemap[] tilemaps)
+    {
+        foreach (var tilemap in tilemaps)
         {
             if (tilemap.gameObject.tag == "groundTilemap")
             {
@@ -56,7 +65,64 @@ public class InstantiatedRoom : MonoBehaviour
                 minimapTilemap = tilemap;
             }
         }
+    }
 
-        collisionTilemap.gameObject.GetComponent<TilemapRenderer>().enabled = false;
+
+    private void BlockOffUnusedDoorways(Tilemap[] tilemaps)
+    {
+        foreach (var doorway in room.doorwayList)
+        {
+            if (doorway.isConnected)
+            {
+                continue;
+            }
+
+            foreach (var tilemap in tilemaps)
+            {
+                if (tilemap == null)
+                {
+                    continue;
+                }
+
+                BlockDoorwayOnTilemap(doorway, tilemap);
+            }
+        }
+    }
+
+    private void BlockDoorwayOnTilemap(Doorway doorway, Tilemap tilemap)
+    {
+        Vector2Int startPosition = doorway.doorwayStartCopyPosition;
+        Vector3Int direction = Vector3Int.zero;
+
+        switch (doorway.orientation)
+        {
+            case Orientation.north:
+            case Orientation.south:
+                direction = Vector3Int.right;
+                break;
+
+            case Orientation.west:
+            case Orientation.east:
+                direction = Vector3Int.down;
+                break;
+
+            default:
+                return;
+        }
+
+        for (int xPos = 0; xPos < doorway.doorwayCopyTileWidth; xPos++)
+        {
+            for (int yPos = 0; yPos < doorway.doorwayCopyTileHeight; yPos++)
+            {
+                var sourceTile = new Vector3Int(startPosition.x + xPos, startPosition.y - yPos);
+                var targetTile = sourceTile + direction;
+
+                var transformMatrix = tilemap.GetTransformMatrix(sourceTile);
+
+                tilemap.SetTile(targetTile, tilemap.GetTile(sourceTile));
+
+                tilemap.SetTransformMatrix(targetTile, transformMatrix);
+            }
+        }
     }
 }
