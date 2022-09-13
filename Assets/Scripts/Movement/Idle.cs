1a3a7bf8d6ac8466d92eb36308911bbe9727a61d
coderKillo <coderKillo@users.noreply.github.com>
2022-09-14 00:28:42 +0200

    add: idle event + handler

---
+++ Assets/Scripts/Movement/Idle.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(IdleEvent))]
+[DisallowMultipleComponent]
+public class Idle : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    private IdleEvent idleEvent;
+
+    private void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        idleEvent = GetComponent<IdleEvent>();
+    }
+
+    private void OnEnable()
+    {
+        idleEvent.OnIdle += IdleEvent_OnIdle;
+    }
+
+    private void OnDisable()
+    {
+        idleEvent.OnIdle -= IdleEvent_OnIdle;
+    }
+
+    private void IdleEvent_OnIdle(IdleEvent obj)
+    {
+        StopMovement();
+    }
+
+    private void StopMovement()
+    {
+        rigidBody2D.velocity = Vector2.zero;
+    }
+}
