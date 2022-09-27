41e7e8e91e30b20a208918f08c59770fe9d84262
coderKillo <coderKillo@users.noreply.github.com>
2022-09-28 00:54:40 +0200

    add: SO for weapon and ammo

---
+++ Assets/Scripts/Weapons/Ammo/AmmoDetailsSO.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AmmoDetails_", menuName = "Scriptable Object/Weapon/Ammo Detail")]
+public class AmmoDetailsSO : ScriptableObject
+{
+    [Space(10)]
+    [Header("AMMO BASE DETAILS")]
+    public string ammoName;
+    public bool isPlayerAmmo;
+
+    [Space(10)]
+    [Header("AMMO SPRITES, PREFABS & MATERIALS")]
+    public Sprite ammoSprite;
+    public GameObject[] prefabArray;
+    public Material ammoMaterial;
+
+    [Space(10)]
+    [Header("AMMO CHARGE")]
+    public float chargeTime = 0.1f;
+    public Material chargeMaterial;
+
+    [Space(10)]
+    [Header("AMMO BASE PARAMETER")]
+    public int damage = 1;
+    public float speedMin = 20f;
+    public float speedMax = 20f;
+    public float range = 20f;
+    public float rotationSpeed = 0f;
+
+    [Space(10)]
+    [Header("AMMO SPREAD DETAILS")]
+    public float spreadMin = 0f;
+    public float spreadMax = 0f;
+
+    [Space(10)]
+    [Header("AMMO SPAWN DETAILS")]
+    public int spawnAmmoMin = 1;
+    public int spawnAmmoMax = 1;
+    public float spawnIntervalMin = 0f;
+    public float spawnIntervalMax = 0f;
+
+    [Space(10)]
+    [Header("AMMO TRAIL DETAILS")]
+    public bool hasTrail = false;
+    public float trailLifetime = 3f;
+    public Material trailMaterial;
+    [Range(0f, 1f)] public float trailStartWidth;
+    [Range(0f, 1f)] public float trailEndWidth;
+
+    #region VALIDATION
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(ammoName), ammoName);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(ammoSprite), ammoSprite);
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(prefabArray), prefabArray);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(ammoMaterial), ammoMaterial);
+
+        if (chargeTime > 0)
+        {
+            HelperUtilities.ValidateCheckNullValue(this, nameof(chargeMaterial), chargeMaterial);
+        }
+
+        HelperUtilities.ValidateCheckPositiveValue(this, nameof(damage), damage, false);
+        HelperUtilities.ValidateCheckPositiveValue(this, nameof(range), range, false);
+        HelperUtilities.ValidateCheckPositiveRange(this, nameof(speedMin), speedMin, nameof(speedMax), speedMax, false);
+        HelperUtilities.ValidateCheckPositiveRange(this, nameof(spreadMin), spreadMin, nameof(spreadMax), spreadMax, true);
+        HelperUtilities.ValidateCheckPositiveRange(this, nameof(spawnAmmoMin), spawnAmmoMin, nameof(spawnAmmoMax), spawnAmmoMax, false);
+        HelperUtilities.ValidateCheckPositiveRange(this, nameof(spawnIntervalMin), spawnIntervalMin, nameof(spawnIntervalMax), spawnIntervalMax, true);
+
+        if (hasTrail)
+        {
+            HelperUtilities.ValidateCheckNullValue(this, nameof(trailMaterial), trailMaterial);
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(trailLifetime), trailLifetime, false);
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(trailStartWidth), trailStartWidth, false);
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(trailEndWidth), trailEndWidth, false);
+        }
+    }
+#endif
+    #endregion
+}
---
+++ Assets/Scripts/Weapons/Weapons/WeaponDetailsSO.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponDetails_", menuName = "Scriptable Object/Weapon/Weapon Detail")]
+public class WeaponDetailsSO : ScriptableObject
+{
+
+    [Space(10)]
+    [Header("WEAPON BASE DETAILS")]
+    public string weaponName;
+    public Sprite weaponSprite;
+
+    [Space(10)]
+    [Header("WEAPON CONFIGURATION")]
+    public Vector3 shootPosition;
+    public AmmoDetailsSO ammo;
+
+    [Space(10)]
+    [Header("WEAPON OPERATION DETAILS")]
+    public bool hasInfiniteAmmo = false;
+    public int ammoCapacity = 100;
+    public bool hasInfiniteClipCapacity = false;
+    public int ammoClipCapacity = 6;
+    public float fireRate = 0.2f;
+    public float prechargeTime = 0f;
+    public float reloadTime = 0f;
+
+    #region VALIDATION
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(weaponName), weaponName);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(ammo), ammo);
+        HelperUtilities.ValidateCheckPositiveValue(this, nameof(fireRate), fireRate, false);
+        HelperUtilities.ValidateCheckPositiveValue(this, nameof(prechargeTime), prechargeTime, true);
+
+        if (!hasInfiniteAmmo)
+        {
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(ammoCapacity), ammoCapacity, false);
+        }
+
+        if (!hasInfiniteClipCapacity)
+        {
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(ammoClipCapacity), ammoClipCapacity, false);
+        }
+    }
+#endif
+    #endregion
+}
