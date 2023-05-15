275e18e27d3b4a1e69c866d4037aa5394d07978a
coderKillo <coderKillo@users.noreply.github.com>
2023-05-15 23:10:59 +0200

    increase level bar over time

--- Assets/Scripts/CardSystem/LevelUI.cs
+++ Assets/Scripts/CardSystem/LevelUI.cs
@@ -8,6 +8,8 @@ public class LevelUI : MonoBehaviour
     [SerializeField] private Transform _levelBar;
     [SerializeField] private TextMeshProUGUI _levelText;
 
+    private float _currentProgress = 0f;
+
     private void OnEnable()
     {
         CardSystemLevel.OnLevelChange += CardSystemLevel_OnLevelChange;
@@ -20,13 +22,22 @@ public class LevelUI : MonoBehaviour
         CardSystemLevel.OnLevelPercentageChange -= CardSystemLevel_OnLevelPercentageChange;
     }
 
+    private void Update()
+    {
+        if (_currentProgress > _levelBar.transform.localScale.x)
+        {
+            _levelBar.transform.localScale += new Vector3(Time.deltaTime, 0f, 0f);
+        }
+    }
+
     private void CardSystemLevel_OnLevelPercentageChange(float pointPercent)
     {
-        _levelBar.transform.localScale = new Vector3(pointPercent, 1f, 1f);
+        _currentProgress = pointPercent;
     }
 
     private void CardSystemLevel_OnLevelChange(int level)
     {
         _levelText.text = level.ToString();
+        _levelBar.transform.localScale = new Vector3(0f, 1f, 1f);
     }
 }
