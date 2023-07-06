89dfdcbc1ef6691f5e7f5c1fd6424e6540a35e0b
coderKillo <coderKillo@users.noreply.github.com>
2023-07-07 00:43:34 +0200

    change card rarity select algorithm and scale chance with level

--- Assets/Scripts/CardSystem/CardSystemSettings.cs
+++ Assets/Scripts/CardSystem/CardSystemSettings.cs
@@ -9,6 +9,24 @@ public class CardSystemSettings : SerializedMonoBehaviour
     [Space(10)]
     [Header("Rarity Ratio")]
     [SerializeField] private Dictionary<CardRarity, int> _rarityRatio;
+    [SerializeField] private float _rationIncreasePerLevel = 1.012f;
+
+    private float _currentRatioIncrease = 1f;
+
+    private void OnEnable()
+    {
+        CardSystemLevel.OnLevelChange += CardSystemLevel_OnLevelChange;
+    }
+
+    private void OnDisable()
+    {
+        CardSystemLevel.OnLevelChange -= CardSystemLevel_OnLevelChange;
+    }
+
+    private void CardSystemLevel_OnLevelChange(int level)
+    {
+        _currentRatioIncrease = Mathf.Pow(_rationIncreasePerLevel, level);
+    }
 
     static public Color GetColor(CardRarity rarity)
     {
@@ -17,14 +35,17 @@ public class CardSystemSettings : SerializedMonoBehaviour
 
     public CardSO PickRandomCard(CardSO[] cards)
     {
-        var rarityListLookup = new List<CardRarity>();
+        var roll = Random.Range(0, 100);
+        var selectedRarity = CardRarity.Common;
+        var sortedRarityRatio = from entry in _rarityRatio orderby entry.Value descending select entry;
 
-        foreach (var item in _rarityRatio)
+        foreach (var item in sortedRarityRatio)
         {
-            rarityListLookup.AddRange(Enumerable.Repeat(item.Key, item.Value));
+            if (roll <= (item.Value * _currentRatioIncrease))
+            {
+                selectedRarity = item.Key;
+            }
         }
-
-        var selectedRarity = rarityListLookup[Random.Range(0, rarityListLookup.Count)];
 
         return PickRandomCardWithSpecificRarity(cards, selectedRarity);
     }
